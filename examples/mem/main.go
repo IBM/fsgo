e16fd2119d4901366c28619cc12692ff458cd90e
John Kevlin <jkevlin@ibm.com>
2022-09-20 15:21:52 -0500

    add example

---
+++ examples/mem/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/IBM/fsgo"
+)
+
+func main() {
+	var fs = fsgo.NewMemMapFs()
+	b := []byte("bar")
+	fs.MkdirAll("some/path/", 0755)
+	err := fsgo.WriteFile(fs, "some/path/foo.txt", b, 0644)
+	if err != nil {
+		panic(err)
+	}
+	err = fs.Remove("some/path/foo.txt")
+	if err != nil {
+		panic(err)
+	}
+	err = fs.RemoveAll("some")
+	if err != nil {
+		panic(err)
+	}
+}
